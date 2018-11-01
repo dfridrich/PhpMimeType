290497c9e36324afbf6462ee17c277ef36f1a249
Travis <travis@travis-ci.org>
2018-11-01 10:07:37 +0000

    API updated

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Defr\\PhpMimeType\\FileAsResponse"],["c","Defr\\PhpMimeType\\MimeType"],["c","Defr\\PhpMimeType\\MimeTypeException"],["c","Defr\\PhpMimeType\\MimeTypeInfo"]];
